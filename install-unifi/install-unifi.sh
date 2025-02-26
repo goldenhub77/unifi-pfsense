2f29f420dee7aaffdfb7a481b6d15d3d8737f62d
Golden <goldenhub77@users.noreply.github.com>
2025-02-26 02:05:28 -0500

    Update install-unifi.sh

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -10,7 +10,7 @@ UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.1.113/UniFi.unix.zip"
 # The rc script associated with this branch or fork:
 RC_SCRIPT_URL="https://raw.githubusercontent.com/unofficial-unifi/unifi-pfsense/master/rc.d/unifi.sh"
 
-CURRENT_MONGODB_VERSION=mongodb60
+CURRENT_MONGODB_VERSION=mongodb50
 
 # If pkg-ng is not yet installed, bootstrap it:
 if ! /usr/sbin/pkg -N 2> /dev/null; then
